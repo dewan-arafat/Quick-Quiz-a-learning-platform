ab6499698bf6e21731ef847efc9ab85ead4f98a9
dewan-arafat <dewanarafat1@gmail.com>
2022-10-12 06:57:06 +0600

    quiz functionality added

--- src/Component/QuizQuestion/QuizQuestion.js
+++ src/Component/QuizQuestion/QuizQuestion.js
@@ -1,31 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './QuizQuestion.css'
 
 
 const QuizQuestion = ({ quiz }) => {
+    const [answer, setAnswer] = useState('');
+    const HandleChange = (e) => {
+        setAnswer(e.target.value);
+        if (answer === quiz.correctAnswer) {
+            alert('Yahooo!!!! Your answer is Correct');
+        } else {
+            alert('Your answer is Wrong');
+        }
+
+    }
+    console.log(quiz);
+
     return (
 
         <div className="my-4 bg-gray-200 p-4 rounded shadow-lg  ">
             <span className="text-gray-700 text-xl pb-2 font-medium">Q. {quiz.question}</span>
             <div className="mt-2">
                 <label className="inline-flex items-center">
-                    <input type="radio" class="form-radio" name="accountType" value={quiz.options[0]} />
+                    <input onChange={HandleChange} type="radio" class="form-radio" name="accountType" value={quiz.options[0]} />
                     <span className="ml-2">{quiz.options[0]}</span>
                 </label><br />
                 <label className="inline-flex items-center">
-                    <input type="radio" class="form-radio" name="accountType" value={quiz.options[1]} />
+                    <input onChange={HandleChange} type="radio" class="form-radio" name="accountType" value={quiz.options[1]} />
                     <span className="ml-2">{quiz.options[1]}</span>
                 </label><br />
                 <label className="inline-flex items-center">
-                    <input type="radio" class="form-radio" name="accountType" value={quiz.options[2]} />
+                    <input onChange={HandleChange} type="radio" class="form-radio" name="accountType" value={quiz.options[2]} />
                     <span className="ml-2">{quiz.options[2]}</span>
                 </label><br />
                 <label className="inline-flex items-center">
-                    <input type="radio" class="form-radio" name="accountType" value={quiz.options[3]} />
+                    <input onChange={HandleChange} type="radio" class="form-radio" name="accountType" value={quiz.options[3]} />
                     <span className="ml-2">{quiz.options[3]}</span>
                 </label>
-
-
             </div>
         </div>
 
--- src/Component/Statistics/Statistics.js
+++ src/Component/Statistics/Statistics.js
@@ -6,26 +6,27 @@ const Statistics = () => {
     const data = useContext(QuizContext)
     console.log(data);
     return (
-        <div className='mt-10 bg-gray-200 my-4 px-4 py-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-4'>
+        <div className='mt-28 bg-gray-200 '>
             <div>
-                <p className='text-center text-orange-400 text-3xl font-semibold pb-5'>Total number of quiz for each topic</p>
+                <p className='text-center text-orange-400 text-3xl font-semibold pt-5 pb-5'>Total number of quiz for each topic</p>
             </div>
-            <div className='flex justify-center'>
-                <LineChart width={500}
-                    height={500}
-                    data={data}
-                    margin={{
-                        top: 50,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                    }}>
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Line type="monotone" dataKey="total" stroke="#82ca9d" />
-                </LineChart>
+            <div className='flex justify-center w-75'>
+                <ResponsiveContainer width="65%" height="50%" aspect={6 / 4}>
+                    <LineChart
+                        data={data}
+                        margin={{
+                            top: 50,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}>
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+                    </LineChart>
+                </ResponsiveContainer>
             </div>
         </div>
     );
